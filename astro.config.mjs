63fc199c899bbf85e0275c909d76d039205aa89a
hermano-padawan <152634122+hermano-padawan@users.noreply.github.com>
2026-08-07 16:15:24 +0200

    Add site and base URL to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://hermano-padawan.github.io",
+  base: "/diu-el-cel",
   output: "static",
-  trailingSlash: "always"
+  trailingSlash: "always",
 });
